872ffb9b9f1bc63e22f35c759b41b494c2b465eb
Xavier CLEMENCE <xavier.clemence@gmail.com>
2020-05-13 00:50:46 +0200

    Try remove string conversion warning

--- src/Dependencies.ApiSetMap/ApiSetMapAnalyserBase.cpp
+++ src/Dependencies.ApiSetMap/ApiSetMapAnalyserBase.cpp
@@ -1,12 +1,21 @@
 #include "stdafx.h"
 
 #include "ApiSetMapAnalyserBase.h"
+#include <codecvt>
+#include <vector>
 
 namespace Dependencies::ApiSetMap
 {
     std::string ApiSetMapAnalyserBase::getString(const wchar_t* message, const ULONG size) const
     {
-        std::wstring wString(message, size / sizeof(wchar_t));
-        return std::string(wString.begin(), wString.end());
+        typedef std::codecvt<wchar_t, char, std::mbstate_t> ConverterType;
+
+        std::locale const loc("");
+        std::size_t const len = size / sizeof(wchar_t);
+        std::vector<char> buffer(len + 1);
+        std::use_facet<std::ctype<wchar_t> >(loc).narrow(message, message + size, '_', &buffer.at(0));
+
+        return std::string(&buffer.at(0), &buffer.at(len));
+
     }
 }
